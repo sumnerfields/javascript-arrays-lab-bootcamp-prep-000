32af5ee8c361d24cf794fa11b676b3d1c7788d0b
Sumner Fields <sumnertfields@gmail.com>
2019-04-29 01:21:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
-destructivelyAppendKitten
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten
