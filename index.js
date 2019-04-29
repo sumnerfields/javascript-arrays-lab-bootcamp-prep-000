81cb4f29bfa583f1bdb462e3ced1c74a51dd3c28
Sumner Fields <sumnertfields@gmail.com>
2019-04-29 01:26:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,4 +16,28 @@ function destructivelyRemoveLastKitten(){
   return kittens
 }
 
-function destructivelyRemoveFirstKitten
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name){
+  var newKittens = [name, ...kittens]
+  return newKittens
+}
+
+function prependKitten(name){
+  var newKittens = [...kittens, name]
+  return newKittens
+}
+
+function removeLastKitten(){
+  var newKittens = kittens.slice(0, kittens.length -1)
+  return newKittens
+}
+
+function removeFirstKitten(){
+  var newKittens = kittens.slice(1)
+  return newKittens
+}
+
